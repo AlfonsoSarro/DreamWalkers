3cad5dd4747b4e9f0dae6bb1ddf997298fa174f5
Alfonso Alejandro Sarró Larrey <alfonso.sarro@students.salle.url.edu>
2022-10-26 03:32:34 +0200

    Added animations for the player
    Added animations for all actions that the player can make

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -8,10 +8,12 @@ public class PlayerController : MonoBehaviour
     private InputAction moveAction; //Store our controls
     public float speed;
     public float jumpHeight;
+    public Animator animator;
     new Rigidbody2D rigidbody;
     
     bool grounded = false;
     bool canDoubleJump = false;
+    bool facingLeft = false;
     private void Awake()
     {
         playerInput = GetComponent<PlayerInput>();
@@ -35,14 +37,18 @@ public class PlayerController : MonoBehaviour
         
         if (grounded || canDoubleJump)
         {
+            animator.SetBool("Grounded", false);
+            animator.SetBool("Falling", false);
             Vector2 velocity = rigidbody.velocity;
             velocity.y = jumpHeight;
-            if(!grounded)
+            if (!grounded)
             {
+                animator.SetBool("DoubleJump", true);
                 canDoubleJump = false;
             }
             else
             {
+                animator.SetBool("Jumping", true);
                 grounded = false;
             }
             rigidbody.velocity = velocity;
@@ -53,6 +59,9 @@ public class PlayerController : MonoBehaviour
     {
         grounded = true;
         canDoubleJump = true;
+        animator.SetBool("Falling", false);
+        animator.SetBool("Grounded", true);
+        animator.SetBool("DoubleJump", false);
     }
     /*
     void Move(InputAction.CallbackContext context)
@@ -67,6 +76,24 @@ public class PlayerController : MonoBehaviour
         Vector2 velocity = rigidbody.velocity;
         velocity.x = moveAction.ReadValue<float>() * speed;
         rigidbody.velocity = velocity;
+        if(velocity.x < 0 && !facingLeft)
+        {
+            transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);
+            facingLeft = true;
+        }
+        if (velocity.x > 0 && facingLeft)
+        {
+            transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);
+            facingLeft = false;
+        }
+        animator.SetFloat("Velocity", Mathf.Abs(velocity.x));
+        if(velocity.y < 0)
+        {
+            grounded = false;
+            animator.SetBool("Falling", true);
+            animator.SetBool("Grounded", false);
+            animator.SetBool("Jumping", false);
+        }
     
         /*
         //Read movement value
