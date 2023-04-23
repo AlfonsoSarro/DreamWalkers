2f62fcafd8e75ae1bd73b4cd90b1de2b887b564c
Marcel Llopis <marcel.llopis@hotmail.com>
2023-04-23 12:30:13 +0200

    Options Menu finished & volume control added

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -5,6 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject playButton;
+    public GameObject optionsButton;
+    public GameObject exitButton;
+    public GameObject returnButton;
+    public GameObject volumeButton;
+    public GameObject slider;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,4 +27,31 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene("SampleScene");
     }
+
+    public void OptionsMenu()
+    {
+        playButton.SetActive(false);
+        optionsButton.SetActive(false);
+        exitButton.SetActive(false);
+        returnButton.SetActive(true);
+        volumeButton.SetActive(true);
+        slider.SetActive(true);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void Return()
+    {
+        playButton.SetActive(true);
+        optionsButton.SetActive(true);
+        exitButton.SetActive(true);
+        returnButton.SetActive(false);
+        volumeButton.SetActive(false);
+        slider.SetActive(false);
+    }
+
 }
+    
